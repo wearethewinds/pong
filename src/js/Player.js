0d783d076b0283b9055d8460a99369b3c979166f
wearethewinds <fpotschies@gmail.com>
2016-05-14 17:50:33 +0200

    [TASK] Moved Player from constructor function to es6-class

--- src/js/Player.js
+++ src/js/Player.js
@@ -1,32 +1,36 @@
 import Bar from './Bar';
 import Result from './Result.js';
 
-var Player = function (orient, keyprofile) {
-    this.points = 0;
-    this.orient = orient;
-    this.keyprofile = keyprofile;
-    this.result = null;
-    this.bar = null;
-};
-
-Player.prototype.createResult = function (gameboard, stage, x, y) {
-    if (this.result) {
-        return;
+class Player {
+    
+    constructor (orient, keyprofile) {
+        this.points = 0;
+        this.orient = orient;
+        this.keyprofile = keyprofile;
+        this.result = null;
+        this.bar = null;
     }
-    this.result = new Result(gameboard, this.points, x, y);
-    this.result.addToStage(stage);
-};
-
-Player.prototype.createBar = function (y, gameboard, stage) {
-    var bar = new Bar(y, this.orient, gameboard, this.keyprofile);
-    bar.addToStage(stage);
-    this.bar = bar;
-    return bar;
-};
-
-Player.prototype.increasePoints = function () {
-    ++this.points;
-    this.result.setPoints(this.points);
-};
+    
+    createBar (y, gameboard, stage) {
+        var bar = new Bar(y, this.orient, gameboard, this.keyprofile);
+        bar.addToStage(stage);
+        this.bar = bar;
+        return bar;
+    }
+    
+    createResult (gameboard, stage, x, y) {
+        if (this.result) {
+            return;
+        }
+        this.result = new Result(gameboard, this.points, x, y);
+        this.result.addToStage(stage);
+    }
+    
+    increasePoints () {
+        ++this.points;
+        this.result.setPoints(this.points);
+    }
+    
+}
 
 module.exports = Player;
